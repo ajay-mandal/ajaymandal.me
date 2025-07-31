739918903f7af0f5e11ee9f347c4c95e6cdf89f0
ajay-mandal <ajayrox48@gmail.com>
2025-08-01 05:04:01 +0530

    date fix

--- data/job.tsx
+++ data/job.tsx
@@ -17,7 +17,7 @@ export const JOBS: JobProps[] = [
         url: "https://juteq.ca/",
         logo: "/juteq.jpeg",
         jobTitle: "Backend Engineer",
-        startDate: "2025-06-24",
+        startDate: "2025-06",
         endDate: "",
         description: `Developing AI Platforms for automotive dealership customer engagement system using NestJS and Prisma ORM. 
         Building scalable backend APIs for orchestrating specialized AI agents. Implementing multi-channel communication systems for
@@ -30,8 +30,8 @@ export const JOBS: JobProps[] = [
         url: "https://klyra.co.in/",
         logo: "/klyra_1.png",
         jobTitle: "Full-Stack Engineer",
-        startDate: "2025-01-13",
-        endDate: "2025-02-13",
+        startDate: "2025-01",
+        endDate: "2025-02",
         description: `Developed the MVP mobile design using React.js and Tailwind CSS, integrating user authentication and email verification with Clerk.
         Engineered a scalable FastAPI backend to handle high-concurrency user requests efficiently.
         Built the organization’s SEO-optimized website using Next.js, React, and TypeScript in collaboration with senior developers.`,
@@ -42,8 +42,8 @@ export const JOBS: JobProps[] = [
         url: "https://www.kyndryl.com/in/en",
         logo: "/KD.svg",
         jobTitle: "Project Intern",
-        startDate: "2024-04-29",
-        endDate: "2024-07-28",
+        startDate: "2024-04",
+        endDate: "2024-07",
         description: `Proactively assisted seniors to enhance service delivery and improve Python scripts.
         Worked with Azure and SQL for data accumulation and visualization.
         Kept projects on track by supporting project managers with progress reports, docs, and more.
--- lib/date.ts
+++ lib/date.ts
@@ -1,7 +1,6 @@
 export function formatDate(date: string) {
-    return new Date(date).toLocaleDateString("en-US", {
-      month: "short",
-      day: "numeric",
-      year: "numeric",
-    });
-  }
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "short",
+    year: "numeric",
+  });
+}
